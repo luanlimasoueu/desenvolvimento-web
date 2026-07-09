3f9d10043d322de2a3cbbbc03df285535e6efa35
Luan Lima <luanemerson8@gmail.com>
2026-07-09 01:28:37 -0300

    Study

---
+++ applications/2_exemplo/meu-app/frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [msg, setMsg] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/")
+      .then((res) => res.json())
+      .then((data) => setMsg(data.mensagem));
+  }, []);
+
+  return (
+    <div>
+      <h1>Meu Site</h1>
+      <h2>{msg}</h2>
+    </div>
+  );
+}
+
+export default App;
