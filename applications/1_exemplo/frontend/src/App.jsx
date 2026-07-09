025a8ed7253da293c90133780613244fa739c99b
Luan Lima <luanemerson8@gmail.com>
2026-07-08 22:55:02 -0300

    study

--- applications/1_exemplo/frontend/src/App.jsx
+++ applications/1_exemplo/frontend/src/App.jsx
@@ -1,23 +1,30 @@
 import { useEffect, useState } from "react";
 
 function App() {
-
-  const [msg, setMsg] = useState("");
+  const [msg, setMsg] = useState("Carregando...");
 
   useEffect(() => {
+    console.log("Chamando API...");
 
     fetch("http://localhost:8000/")
-      .then(res => res.json())
-      .then(data => setMsg(data.mensagem));
-
+      .then((res) => {
+        console.log("Status:", res.status);
+        return res.json();
+      })
+      .then((data) => {
+        console.log("Resposta:", data);
+        setMsg(data.mensagem);
+      })
+      .catch((err) => {
+        console.error("Erro:", err);
+        setMsg("Erro ao conectar");
+      });
   }, []);
 
   return (
     <div>
       <h1>Meu Site</h1>
-
       <h2>{msg}</h2>
-
     </div>
   );
 }
